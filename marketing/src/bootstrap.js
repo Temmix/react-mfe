ba68391b67a074dffa242c963d7caf9b6dd5d359
Temi Makinde <Temi.Makinde@Gmail.com>
2021-02-01 23:38:45 +0000

    Initial commits

---
+++ marketing/src/bootstrap.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import App from './App';
+
+const mount = (element) => {
+  ReactDOM.render(<App />, element);
+};
+
+//for internal development purpose
+if (process.env.NODE_ENV === 'development') {
+  const devElement = document.querySelector('#marketing-dev-root');
+  if (devElement) mount(devElement);
+}
+
+// for external production purpose
+export { mount };

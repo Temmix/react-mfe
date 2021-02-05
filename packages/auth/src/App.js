b5def3011a412ebf80d8e93b022688dcf49b89dd
Temi Makinde <Temi.Makinde@Gmail.com>
2021-02-05 00:10:16 +0000

    Reverifying the auth sub app

--- packages/auth/src/App.js
+++ packages/auth/src/App.js
@@ -9,7 +9,7 @@ import Signin from './components/Signin';
 import Signup from './components/Signup';
 
 const generateClassName = createGenerateClassName({
-  productionPrefix: 'au',
+  productionPrefix: 'aux',
 });
 
 export default ({ history, onSignIn }) => {
